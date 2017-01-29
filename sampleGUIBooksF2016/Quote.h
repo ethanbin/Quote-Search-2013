1cf60e2a4b46899ccc641021bafc9b91947189c2
Ethan B <ethanbinya@aol.com>
2017-01-29 14:16:55 -0500

    added comments to each constructor and function in Quote.h

--- sampleGUIBooksF2016/Quote.h
+++ sampleGUIBooksF2016/Quote.h
@@ -3,13 +3,28 @@
 class Quote
 {
 public:
+	//defaults to "0" for every variable
 	Quote();
-	Quote(std::string, std::string, Author); //accepts quote then theme then author's info
+
+	//copy constructor taking string quote, string theme, and Author authorInfo
+	Quote(std::string, std::string, Author);
+
+	//copy constructor
 	void copy(Quote);
+
+	//returns string formatted for display to user
 	std::string fullDisplay() const;
+
+	//returns _quote
 	std::string returnQuote();
+
+	//returns _year from _authorInfo
 	std::string returnYear();
+
+	//returns _theme
 	std::string returnTheme();
+
+	//returns _author from _authorInfo
 	std::string returnQuoteName();
 	
 private:
